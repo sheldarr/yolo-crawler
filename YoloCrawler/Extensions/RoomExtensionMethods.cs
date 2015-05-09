4aa94dc29586fcc1b87be55b157472f3d2a4dcae
sheldarr <sheldar@wp.pl>
2015-05-09 15:28:25 +0200

    Moved missing logic to monster factory.

--- YoloCrawler/Extensions/RoomExtensionMethods.cs
+++ YoloCrawler/Extensions/RoomExtensionMethods.cs
@@ -2,18 +2,12 @@
 {
     using Entities;
     using Factories;
-    using Fighting;
-    using Input;
 
     public static class RoomExtensionMethods
     {
         public static Room WithRandomMonster(this Room room, Position position)
         {
-            var monsters = MonsterNamesLoader.Load();
-            var randomMonsterNameIndex = RandomGenerator.Random.Next(1, monsters.Count);
-
-            var monsterName = monsters[randomMonsterNameIndex];
-            var monster = new Monster(monsterName, position);
+            var monster = MonsterFactory.CreateRandomMonster(room, position);
 
             room.Monsters.Add(monster);
 
--- YoloCrawler/Factories/MonsterFactory.cs
+++ YoloCrawler/Factories/MonsterFactory.cs
@@ -1,12 +1,18 @@
 ﻿namespace YoloCrawler.Factories
 {
     using Entities;
+    using Fighting;
 
     public static class MonsterFactory
     {
-        public static Monster CreateRandomMonster(Position position)
+        public static Monster CreateRandomMonster(Room room, Position position)
         {
-            return new Monster("Clayton", position);
+            var monsters = MonsterNamesLoader.Load();
+            var randomMonsterNameIndex = RandomGenerator.Random.Next(1, monsters.Count);
+
+            var monsterName = monsters[randomMonsterNameIndex];
+            
+            return new Monster(monsterName, position);
         }
     }
 }
