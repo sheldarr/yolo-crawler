2bc87f42c6df590b270b59392c4bb336e6e91f2d
TheOneYouDontLike <theoneyoudontlike@gmail.com>
2015-05-09 08:41:48 +0200

    Added test project.

---
+++ YoloCrawler.Tests/Class1.cs
@@ -0,0 +1,18 @@
+﻿namespace YoloCrawler.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Should_method_name()
+        {
+            // given
+
+            // when
+
+            // then
+        }
+    }
+}
