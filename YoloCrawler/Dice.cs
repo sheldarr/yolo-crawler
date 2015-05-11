9b4d69cc9d0eeaa0a36f001b709c0424a766a50f
TheOneYouDontLike <theoneyoudontlike@gmail.com>
2015-05-11 21:35:53 +0200

    tests for dice and i hate M$ for their stupid decisions #12

--- YoloCrawler/Dice.cs
+++ YoloCrawler/Dice.cs
@@ -5,6 +5,7 @@
     public class Dice
     {
         private readonly Random _random;
+        private const int FixingStupidUpperBound = 1;
 
         public Dice()
         {
@@ -13,53 +14,53 @@
 
         public int RollK100()
         {
-            return _random.Next(1, 100);
+            return _random.Next(1, 100 + FixingStupidUpperBound);
         }
 
-        public int RollForPlaceOnTheWall(int roomHeight)
+        public int RollForPlaceOnTheWall(int roomWallSize)
         {
             const int minDistanceToAvoidTheCorner = 1;
-            return _random.Next(minDistanceToAvoidTheCorner, roomHeight - minDistanceToAvoidTheCorner);
+            return _random.Next(minDistanceToAvoidTheCorner, roomWallSize - minDistanceToAvoidTheCorner);
         }
 
-        public int RollForFreeAvailableCoordinateValueBasedOn(int widthOrHeight)
+        public int RollForFreeAvailableCoordinateValueBasedOn(int roomWallSize)
         {
             const int minDistanceToWall = 2;
-            return _random.Next(minDistanceToWall, widthOrHeight - minDistanceToWall);
+            return _random.Next(minDistanceToWall, roomWallSize - minDistanceToWall);
         }
 
         public int RollForNeighboursCount(int minCount, int maxCount)
         {
-            return _random.Next(minCount, maxCount);
+            return _random.Next(minCount, maxCount + FixingStupidUpperBound);
         }
 
         public int RollForRandomRoomWidth()
         {
             const int minRoomWidth = 4;
             const int maxRoomWidthBasedOnMaxDisplayWidth = 58;
-            return _random.Next(minRoomWidth, maxRoomWidthBasedOnMaxDisplayWidth);
+            return _random.Next(minRoomWidth, maxRoomWidthBasedOnMaxDisplayWidth + FixingStupidUpperBound);
         }
 
         public int RollForRandomRoomHeight()
         {
             const int minRoomHeight = 4;
             const int maxRoomHeightBasedOnMaxDisplayHeight = 18;
-            return _random.Next(minRoomHeight, maxRoomHeightBasedOnMaxDisplayHeight);
-        }
-
-        public int RollForRandomRoomIndex(int roomCount)
-        {
-            return _random.Next(0, roomCount - 1);
+            return _random.Next(minRoomHeight, maxRoomHeightBasedOnMaxDisplayHeight + FixingStupidUpperBound);
         }
 
         public int RollForHitpoints(int minHitpoints, int maxHitpoints)
         {
-            return _random.Next(minHitpoints, maxHitpoints);
+            return _random.Next(minHitpoints, maxHitpoints + FixingStupidUpperBound);
+        }
+
+        public int RollForRandomRoomIndex(int roomCount)
+        {
+            return _random.Next(0, roomCount);
         }
 
         public int RollForMonsterNameIndex(int monstersCount)
         {
-            return _random.Next(1, monstersCount);
+            return _random.Next(0, monstersCount);
         }
     }
 }
